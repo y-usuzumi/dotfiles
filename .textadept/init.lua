c1ce7110d59251199240f96636f32e7706b1c66a
Kenneth Zhao <kenneth.zhao@netis.com.cn>
2017-11-17 20:14:46 +0800

    TextAdept -- Fix caret and scrolling

--- .textadept/init.lua
+++ .textadept/init.lua
@@ -7,7 +7,6 @@ io.quick_open_max = 100000
 
 
 -- Theming
-buffer.caret_width = 2
 if CURSES then
 else
   ui.set_theme('light', {font = 'Hack', fontsize = 12})
@@ -15,4 +14,9 @@ end
 
 events.connect(events.INITIALIZED, function() 
   myeditor.hide_menu_bar()
+end)
+
+events.connect(events.BUFFER_NEW, function()
+  buffer.caret_width = 2
+  buffer.end_at_last_line = false
 end)
