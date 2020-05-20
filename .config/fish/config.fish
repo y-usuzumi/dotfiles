56531293c33f9fe12d5cab5668ed36851160e6de
Yukio Usuzumi <anohigisavay@gmail.com>
2020-05-20 16:14:24 -0700

    +

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,15 @@
 set -U fish_greeting ""
-set -U fish_user_paths ~/.local/bin ~/.cargo/bin ~/.cabal/bin ~/.ghcup/bin ~/go/bin ~/.emacs.d/bin
+set -U fish_user_paths ~/.local/bin ~/.cargo/bin ~/.cabal/bin ~/.ghcup/bin ~/go/bin ~/.emacs.d/bin ~/.yarn/bin
 set -Ux RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
 set -Ux RUSTUP_UPDATE_ROOT https://mirrors.ustc.edu.cn/rust-static/rustup
 set -Ux GOPRIVATE "*.netisdev.com"
+set -Ux EDITOR nvim
 
 set -g fish_prompt_pwd_dir_length 0
 
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme dracula
+# set -g theme_color_scheme gruvbox
 
 eval (python -m virtualfish auto_activation global_requirements compat_aliases)
 
@@ -24,14 +26,33 @@ function nvm
     bass source /usr/share/nvm/init-nvm.sh --no-use ';' nvm $argv
 end
 
+function _wine32prefix
+    env WINEPREFIX=$HOME/.wine32 $argv
+end
+
+function wine32
+    _wine32prefix wine $argv
+end
+
+function winetricks32
+    _wine32prefix winetricks $argv
+end
+
+function regedit32
+    _wine32prefix regedit $argv
+end
+
+function winecfg32
+    _wine32prefix winecfg
+end
+
 if test "$DESKTOP_SESSION" = "/usr/share/xsessions/i3"
     set -x QT_QPA_PLATFORMTHEME "qt5ct"
     set -x TERMINAL "konsole"
 end
 
-set -l ppid (cat /proc/(echo %self)/status | grep PPid | cut -f2)
-if string match -r 'tilix' (ps --no-headers -p $ppid -o comm) 1> /dev/null
-    for wid in (xdotool search --pid $ppid)
-        xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id $wid
-    end
+# https://github.com/y-usuzumi/j
+function j
+    eval (eval ".j $argv")
 end
+
